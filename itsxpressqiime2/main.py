44702c5162aacf855a2f2e1b52fa8bb81c17a9d8
kweber <kweber1@ufl.edu>
2018-06-23 16:45:27 -0400

    Changed the main.py completely.

--- itsxpressqiime2/main.py
+++ itsxpressqiime2/main.py
@@ -1,69 +1,47 @@
+from q2_types.per_sample_sequences import SingleLanePerSamplePairedEndFastqDirFmt, SingleLanePerSampleSingleEndFastqDirFmt, FastqGzFormat, FastqManifestFormat, YamlFormat
+import os, yaml
 from itsxpress import main as itsx
-from q2_types.per_sample_sequences import SingleLanePerSamplePairedEndFastqDirFmt, FastqGzFormat, FastqManifestFormat, YamlFormat, SingleLanePerSampleSingleEndFastqDirFmt
-import os
-import gzip
-import yaml
-from itsxpress.definitions import ROOT_DIR, taxa_choices, taxa_dict
-from q2_types.feature_data import FeatureData
-
-def _read_sequences(ID, mainPath):
-
-    fn = gzip.open('{}/{}'.format(mainPath,ID))
-
-    return fn
+from itsxpress.definitions import ROOT_DIR, taxa_dict
 
 
+def _view_artifact_type(qzaPath):
 
-def _artifact_type_finder(mainPath):
+    try:
 
+        path = str(qzaPath[:-4]) + "/metadata.yaml"
 
+        fnOpen = open(path, "r")
 
-    PEWSO = False
-    SWQ = False
-    JSWO = False
+        for line in fnOpen:
 
-    fn = (mainPath[:-4] + "/metadata.yaml")
+            if 'type:' in line:
 
-    fnOpened= open(fn, "r")
+                artifactType = line[6:]
 
-    for lines in fnOpened:
+                break
 
-        if 'type' in lines:
+        fnOpen.close()
 
-            artifactType = lines[6:]
+        return artifactType
 
-            break
+    except:
 
-    if '[PairedEndSequencesWithQuality]' in artifactType:
-
-        PEWSO = True
-
-    elif '[SequencesWithQuality]' in artifactType:
-
-        SWQ = True
-
-    elif '[JoinedSequencesWithQuality]' in artifactType:
-
-        JSWO = True
-
-    else:
-
-        raise ValueError("Incorrect artifact type given!")
-
-    fnOpened.close()
-
-    return PEWSO, SWQ, JSWO
+        raise ValueError("The metadata file of the QZA you entered is missing or the 'type' in the file is missing.")
 
 
+def _amount_of_files_in_data(qzaPath):
 
+    try:
+        fnlist = list(os.listdir(qzaPath))
 
-def _amount_of_files(mainPath):
+        fnNumber = len(fnlist)
 
-    fnlist = list(os.listdir(mainPath))
+        return fnNumber
 
-    fnNumber = len(fnlist)
+    except:
 
-    return fnNumber
+        raise ValueError("Internal error with file sequence file counting.")
+
 
 def _write_metadata(results):
 
@@ -71,135 +49,151 @@ def _write_metadata(results):
     metadata.path.write_text(yaml.dump({'phred-offset': 33}))
     results.metadata.write_data(metadata, YamlFormat)
 
+def _fastq_id_maker(per_sample_sequences):
 
-
-#The main function to set up and run itsxpress
-
-def trim2(per_sample_sequences: SingleLanePerSamplePairedEndFastqDirFmt,
-         region: str,
-         taxa: str,
-         threads: int)-> FeatureData:
-
-    results = FeatureData()
-
-    return results
-
-
-
-def trim(per_sample_sequences: SingleLanePerSamplePairedEndFastqDirFmt,
-         region: str,
-         taxa: str,
-         threads: int)-> SingleLanePerSampleSingleEndFastqDirFmt:
-    try:
-
-        #setup for ITSxpress
-
-        single_end = False
-
-        mainPath = str(per_sample_sequences.path)
-
-        PEWSOType, SWQType, JSWQType = _artifact_type_finder(mainPath)
-
-        fnNum = _amount_of_files(mainPath)
 
         iterator = per_sample_sequences.sequences.iter_views(FastqGzFormat)
 
-
         sampleIds = []
-        for id, (fname,fp) in enumerate(iterator):
+
+        for id, (fname, fp) in enumerate(iterator):
 
             sampleIds.append(str(fname))
 
-
-        if (PEWSOType
-            and  fnNum == 4):
-
-            fastq = mainPath + "/" + str(sampleIds[0])
-
-            fastq2 = mainPath + "/" + str(sampleIds[1])
+        return sampleIds
 
 
-        elif (SWQType
-
-            and fnNum == 3):
-
-            fastq = mainPath + "/" + str(sampleIds[0])
-
-            fastq2 = None
-
-            single_end = True
-
-        elif (JSWQType
-            and fnNum == 3):
-
-            fastq = mainPath + "/" + str(sampleIds[0])
-
-            fastq2 = None
-
-        else:
-
-            raise ValueError("The artifact type and number of sequence files inside do not match.")
+def _set_fastq_files(artifactType, qzapath, per_sample_sequences):
 
 
-        #running ITSxpress
+    seqAmount = int(_amount_of_files_in_data(qzapath)) - 2
+
+    sequences = _fastq_id_maker(per_sample_sequences)
+
+    singleEnd = False
+
+    sample_id = sequences[0]
+
+    if (("SampleData[PairedEndSequencesWithQuality]" in artifactType)
+        and (seqAmount == 2)):
+
+        try:
+            fastq = qzapath + "/" + sequences[0]
+
+            fastq2 = qzapath + "/" + sequences[1]
+        except:
+
+            raise ValueError("Invalid Sequence amount for type.")
 
 
+    elif (("SampleData[SequencesWithQuality]" in artifactType)
+        and (seqAmount == 1)):
+
+        fastq = qzapath + "/" + sequences[0]
+
+        fastq2 = None
+
+        singleEnd = True
+
+    elif (("SampleData[JoinedSequencesWithQuality]" in artifactType)
+        and (seqAmount == 1)):
+
+        fastq = qzapath + "/" + sequences[0]
+
+        fastq2 = None
+
+    else:
+
+        raise ValueError("The QZA file and its type '{}' you entered contains more than the amount of sequence files for the ITSxpress program. ".format(artifactType),artifactType)
+
+    return fastq, fastq2, singleEnd, sample_id
+
+
+def trim(per_sample_sequences: SingleLanePerSampleSingleEndFastqDirFmt,
+         region: str,
+         taxa: str,
+         threads: int)-> SingleLanePerSampleSingleEndFastqDirFmt:
+
+
+
+
+    qzaPath = str(per_sample_sequences.path)
+
+    artifactType = _view_artifact_type(qzaPath)
+
+    fastq, fastq2, singleEnd, sample_id = _set_fastq_files(artifactType, qzaPath, per_sample_sequences)
+
+    #Ruuning ITSxpress
+
+    try:
         itsx._check_fastqs(fastq, fastq2)
+        paired_end, interleaved = itsx._is_paired(fastq, fastq2, singleEnd)
 
-        paired_end, interleaved = itsx._is_paired(fastq, fastq2, single_end)
-
-
+    except:
+        raise ValueError("There is a problem with the fastq file you selected or\n"
+                         "BBTools is not installed on this system.\n"
+                         "Please check them before running the program again.")
+    try:
         if paired_end and interleaved:
 
-            sobj = itsx.SeqSamplePairedInterleaved(fastq=fastq, tempdir="./tmp")
+            sobj = itsx.SeqSamplePairedInterleaved(fastq=fastq, tempdir="/tmp")
 
-            sobj.itsx_merge_reads(threads=threads)
+            sobj._merge_reads(threads=threads)
 
         elif paired_end and not interleaved:
 
-            sobj = itsx.SeqSamplePairedNotInterleaved(fastq=fastq, fastq2=fastq2 ,tempdir= "./tmp")
+            sobj = itsx.SeqSamplePairedNotInterleaved(fastq=fastq, fastq2=fastq2,tempdir= "/tmp")
 
             sobj._merge_reads(threads=threads)
 
         elif not paired_end and not interleaved:
 
-            sobj = itsx.SeqSampleNotPaired(fastq=fastq, tempdir="./tmp")
+            sobj = itsx.SeqSampleNotPaired(fastq=fastq, tempdir="/tmp")
+    except:
 
+        raise ValueError("BBmerge is not installed on this system.")
 
+    sobj._deduplicate(threads=threads)
 
-        sobj._deduplicate(threads=threads)
+    try:
 
         hmmfile = os.path.join(ROOT_DIR, "ITSx_db", "HMMs", taxa_dict[taxa])
 
         sobj._search(hmmfile=hmmfile, threads=threads)
 
-
-        its_pos = itsx.ItsPosition(domtable=sobj.dom_file, region=region)
-
-        dedup_obj = itsx.Dedup(uc_file=sobj.uc_file, rep_file=sobj.rep_file, seq_file=sobj.seq_file)
-
-        results = SingleLanePerSampleSingleEndFastqDirFmt()
-
-
-        path = results.path
-
-
-
-        manifest = FastqManifestFormat()
-        manifest_fn = manifest.open()
-        manifest_fn.write('sample-id,filename,direction\n')
-        manifest_fn.write("{},{},reverse".format(sampleIds[0],path.name))
-        manifest_fn.close()
-        dedup_obj.create_trimmed_seqs((str(path)+"/"+sampleIds[0]),gzipped=True, itspos=its_pos)
-        results.manifest.write_data(manifest, FastqManifestFormat)
-        _write_metadata(results)
-
     except:
-        raise ValueError("error")
+
+        raise ValueError("hmmsearch was not found, make sure HMMER3 is installed and executible")
+
+    its_pos = itsx.ItsPosition(domtable=sobj.dom_file, region=region)
+
+    dedup_obj = itsx.Dedup(uc_file=sobj.uc_file, rep_file=sobj.rep_file, seq_file=sobj.seq_file)
+
+    results = SingleLanePerSampleSingleEndFastqDirFmt()
+
+    path = results.sequences.path_maker(sample_id="seq",
+                                        barcode_id=1,
+                                        lane_number=1,
+                                        read_number=1)
+
+    manifest = FastqManifestFormat()
+
+    manifest_fn = manifest.open()
+
+    manifest_fn.write('sample-id,filename,direction\n')
+
+    manifest_fn.write("seq,{},reverse".format(path))
+
+    manifest_fn.close()
+
+
+    dedup_obj.create_trimmed_seqs(str(path), gzipped=True, itspos=its_pos)
+
+    _write_metadata(results)
+
+    results.manifest.write_data(manifest, FastqManifestFormat)
+
+    itsx.shutil.rmtree(sobj.tempdir)
+
 
     return results
-
-
-
-
-
--- itsxpressqiime2/plugin_setup.py
+++ itsxpressqiime2/plugin_setup.py
@@ -3,7 +3,7 @@ from q2_types.sample_data import SampleData
 from q2_types.per_sample_sequences import SequencesWithQuality, PairedEndSequencesWithQuality, JoinedSequencesWithQuality
 from itsxpress import definitions as taxa
 from qiime2.plugin import Plugin, Str, Choices, Int
-from itsxpressqiime2.main import trim, trim2
+from itsxpressqiime2.main import trim
 from q2_types.feature_data import FeatureData, AlignedSequence
 
 plugin = Plugin(
@@ -18,35 +18,18 @@ plugin = Plugin(
 
 plugin.methods.register_function(
     function=trim,
-    inputs={'per_sample_sequences': SampleData[PairedEndSequencesWithQuality]},
+    inputs={'per_sample_sequences': SampleData[PairedEndSequencesWithQuality| SequencesWithQuality | JoinedSequencesWithQuality]},
     parameters={'region': Str %Choices(['ITS2','ITS1','ALL']),
                 'taxa': Str %Choices(taxa.taxa_choices),
                 'threads': Int},
-    outputs=[('per_sample_sequences', SampleData[SequencesWithQuality])],
+    outputs=[('sample', SampleData[SequencesWithQuality])],
     input_descriptions={'per_sample_sequences': 'The artifact that contains the sequence files.'},
     parameter_descriptions={
         'region': ('The regions ITS2, ITS1, and ALL.'),
         'taxa': ('The selected taxonomic group sequenced.'),
         'threads': ('The number of processor threads to use.')
     },
-    output_descriptions={'per_sample_sequences': 'The resulting trimmed sequences.'},
-    name='Trim',
-    description='ITSxpress is deigned to.....'
-)
-plugin.methods.register_function(
-    function=trim2,
-    inputs={'per_sample_sequences': SampleData[PairedEndSequencesWithQuality]},
-    parameters={'region': Str %Choices(['ITS2','ITS1','ALL']),
-                'taxa': Str %Choices(taxa.taxa_choices),
-                'threads': Int},
-    outputs=[('masked_alignment', FeatureData[AlignedSequence])],
-    input_descriptions={'per_sample_sequences': 'The artifact that contains the sequence files.'},
-    parameter_descriptions={
-        'region': ('The regions ITS2, ITS1, and ALL.'),
-        'taxa': ('The selected taxonomic group sequenced.'),
-        'threads': ('The number of processor threads to use.')
-    },
-    output_descriptions={'masked_alignment': 'The resulting trimmed sequences.'},
+    output_descriptions={'sample': 'The resulting trimmed sequences.'},
     name='Trim',
     description='ITSxpress is deigned to.....'
 )
