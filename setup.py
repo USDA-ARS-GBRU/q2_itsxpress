35ff5f6619154b401d5c3b6afdf69f3c2beaf335
kweber <kweber1@ufl.edu>
2018-06-25 23:30:39 -0400

    Version 1.1 is ready! Non pre-release anymore

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='itsxpressqiime2',
-    version='1.07',
+    version='1.10',
     packages=['itsxpressqiime2'],
     author='Kyle Weber',
     author_email='kweber1@ufl.edu',
