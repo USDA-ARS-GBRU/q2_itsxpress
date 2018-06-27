6decd3c3f7c4a351178a90229d1c7d77f976637b
kweber1 <kweber1@ufl.edu>
2018-06-27 16:33:16 -0400

    First step in allowing it to accept more than 1 pair of files

--- itsxpressqiime2/main.py
+++ itsxpressqiime2/main.py
@@ -59,28 +59,11 @@ def _view_artifact_type():
                 break
 
         fnOpen.close()
+
         return artifactType
     except:
         raise ValueError("The metadata file of the qza you entered is missing or the 'type:' in the file is missing.")
 
-
-def _amount_of_files_in_data(qzaPath):
-
-    """Finds the number of files in the data folder of the qza.
-
-    Args:
-
-        qzaPath (int): The path of the per_sequence_sample.
-
-    Returns:
-
-        (int): The number of files in the data folder.
-
-    """
-
-    fnlist = list(os.listdir(qzaPath))
-    fnNumber = len(fnlist)
-    return fnNumber
 
 def _write_metadata(results):
 
@@ -96,7 +79,7 @@ def _write_metadata(results):
     metadata.path.write_text(yaml.dump({'phred-offset': 33}))
     results.metadata.write_data(metadata, YamlFormat)
 
-def _fastq_id_maker(per_sample_sequences):
+def _fastq_id_maker(per_sample_sequences, artifactType):
 
     """Iterates thru the sequences files to get the file path/name.
 
@@ -104,74 +87,49 @@ def _fastq_id_maker(per_sample_sequences):
 
         per_sample_sequences (SingleLanePerSampleSingleEndFastqDirFmt): The SingleLanePerSampleSingleEndFastqDirFmt type
         of the input.
+        artifactType (str): The artifact type in the metadata file.
 
     Returns:
 
-        (list): The path/name of the sequences.
+        (zip lists): The path/name of the sequences.
+        (bool): If single end is true or false
 
     """
 
-    iterator = per_sample_sequences.sequences.iter_views(FastqGzFormat)
-    sampleIds = []
-
-    for ids, (fname, fp) in enumerate(iterator):
-        sampleIds.append(str(fname))
-        # useless code, just to call the ids, fp and holder.
-        holder = ids, fp
-        holder = holder
-
-    return sampleIds
-
-def _set_fastq_files(artifactType, per_sample_sequences):
-
-    """Gives the one or two fastq files that the ITSxpress program will be handed.
-
-    Args:
-
-        artifactType (str) : The artifact type in the metadata file.
-        per_sample_sequences (SingleLanePerSampleSingleEndFastqDirFmt): The SingleLanePerSampleSingleEndFastqDirFmt type
-        of the input.
-
-    Returns:
-
-        (str): The first fastq location.
-        (str): The second fastq location.
-        (bool): Is its single-end or not.
-
-    Raises:
-
-        ValueError1: The sequences files in the qza is an invalid amount.
-        ValueError2: The qza is a invalid type or the number of files is invalid
-
-    """
-
-    seqAmount = int(_amount_of_files_in_data(str(per_sample_sequences.path))) - 2
-    sequences = _fastq_id_maker(per_sample_sequences)
+    path = os.path.join(str(per_sample_sequences.path), "MANIFEST")
+    fn = open(path, "r")
+    sampleForward = []
+    sampleReverse = []
     singleEnd = False
+    for line in fn:
 
-    if (("SampleData[PairedEndSequencesWithQuality]" in artifactType)
-        and (seqAmount == 2)):
-        try:
-            fastq = os.path.join(str(per_sample_sequences.path), str(sequences[0]))
-            fastq2 = os.path.join(str(per_sample_sequences.path), str(sequences[1]))
-        except:
-            raise ValueError("Invalid Sequence amount for type.")
+        if line[0] == "#":
+            continue
+        elif line == "sample-id,filename,direction":
+            continue
+        elif ",forward" in line:
+            line = line.split(",")
+            sampleForward.append(line[1].replace(",",""))
+        elif ",reverse" in line:
+            if artifactType == "SampleData[PairedEndSequencesWithQuality]":
+                line = line.split(",")
+                sampleReverse.append(line[1].replace(",",""))
+            else:
+                line = line.split(",")
+                sampleForward.append(line[1].replace(",",""))
+                sampleReverse.append(None)
 
-    elif (("SampleData[SequencesWithQuality]" in artifactType)
-        and (seqAmount == 1)):
-        fastq = os.path.join(str(per_sample_sequences.path), str(sequences[0]))
-        fastq2 = None
-        singleEnd = True
-
-    elif (("SampleData[JoinedSequencesWithQuality]" in artifactType)
-        and (seqAmount == 1)):
-        fastq = os.path.join(str(per_sample_sequences.path), str(sequences[0]))
-        fastq2 = None
+    if (len(sampleForward) != len(sampleReverse)
+        and (artifactType == "SampleData[PairedEndSequencesWithQuality]")):
+        raise ValueError("The number of forward and reverse samples do not match.")
 
     else:
-        raise ValueError("The qza and its type '{}' you entered contains more than\n"
-                         "the amount of sequence files for the ITSxpress program. ".format(artifactType))
-    return fastq, fastq2, singleEnd
+        sampleIds = zip(sampleForward, sampleReverse)
+        if ("SampleData[SequencesWithQuality]" in artifactType):
+            singleEnd = True
+
+    return sampleIds, singleEnd
+
 
 # The ITSxpress handling
 def _taxa_prefix_to_taxa(taxa_prefix):
@@ -221,65 +179,71 @@ def main(per_sample_sequences, threads, taxa, region):
     os.chdir(str(per_sample_sequences.path))
     # Finding the artifact type.
     artifactType = _view_artifact_type()
-    # Setting the fastq files and if singleEnd is used.
-    fastq, fastq2, singleEnd = _set_fastq_files(artifactType, per_sample_sequences)
     # setting the taxa
     taxa = _taxa_prefix_to_taxa(taxa)
-    # Running the main ITSxpress program.
-    try:
-        itsx._check_fastqs(fastq, fastq2)
-        # Parse input types
-        paired_end, interleaved = itsx._is_paired(fastq, fastq2, singleEnd)
-    except:
-        raise ValueError("There is a problem with the fastq file(s) you selected or\n"
-                         "BBtools was not found. check that the BBtools reformat.sh package is executable.")
-    # Create SeqSample objects and merge if needed.
-    try:
-        if paired_end and interleaved:
-            sobj = itsx.SeqSamplePairedInterleaved(fastq=fastq, tempdir=dirt)
-            sobj._merge_reads(threads=threads)
-
-        elif paired_end and not interleaved:
-            sobj = itsx.SeqSamplePairedNotInterleaved(fastq=fastq, fastq2=fastq2, tempdir=dirt)
-            sobj._merge_reads(threads=threads)
-
-        elif not paired_end and not interleaved:
-            sobj = itsx.SeqSampleNotPaired(fastq=fastq, tempdir=dirt)
-
-    except:
-        raise ValueError("BBmerge was not found. check that the BBmerge reformat.sh package is executible")
-
-    # Deduplicate
-    sobj._deduplicate(threads=threads)
-    try:
-        # HMMSearch for ITS regions
-        hmmfile = os.path.join(ROOT_DIR, "ITSx_db", "HMMs", taxa_dict[taxa])
-        sobj._search(hmmfile=hmmfile, threads=threads)
-    except:
-        raise ValueError("hmmsearch was not found, make sure HMMER3 is installed and executible")
-
-    # Parse HMMseach output.
-    its_pos = itsx.ItsPosition(domtable=sobj.dom_file, region=region)
-    # Create deduplication object.
-    dedup_obj = itsx.Dedup(uc_file=sobj.uc_file, rep_file=sobj.rep_file, seq_file=sobj.seq_file)
-    results = SingleLanePerSampleSingleEndFastqDirFmt()
-    path = results.sequences.path_maker(sample_id="seq",
-                                        barcode_id=1,
-                                        lane_number=1,
-                                        read_number=1)
     # Writing the manifest for the output qza
     manifest = FastqManifestFormat()
     manifest_fn = manifest.open()
     manifest_fn.write('sample-id,filename,direction\n')
-    manifest_fn.write("seq,{},forward".format(path.name))
-    manifest_fn.close()
-    # Create trimmed sequences.
-    dedup_obj.create_trimmed_seqs(str(path), gzipped=True, itspos=its_pos)
+    sequences = _fastq_id_maker(per_sample_sequences, artifactType)
+
+    k = True
+    if k:
+
+        # Setting the fastq files and if singleEnd is used.
+        fastq, fastq2, singleEnd = _set_fastq_files(artifactType, per_sample_sequences)
+        # Running the main ITSxpress program.
+        try:
+            itsx._check_fastqs(fastq, fastq2)
+            # Parse input types
+            paired_end, interleaved = itsx._is_paired(fastq, fastq2, singleEnd)
+        except:
+            raise ValueError("There is a problem with the fastq file(s) you selected or\n"
+                             "BBtools was not found. check that the BBtools reformat.sh package is executable.")
+        # Create SeqSample objects and merge if needed.
+        try:
+            if paired_end and interleaved:
+                sobj = itsx.SeqSamplePairedInterleaved(fastq=fastq, tempdir=dirt)
+                sobj._merge_reads(threads=threads)
+
+            elif paired_end and not interleaved:
+                sobj = itsx.SeqSamplePairedNotInterleaved(fastq=fastq, fastq2=fastq2, tempdir=dirt)
+                sobj._merge_reads(threads=threads)
+
+            elif not paired_end and not interleaved:
+                sobj = itsx.SeqSampleNotPaired(fastq=fastq, tempdir=dirt)
+
+        except:
+            raise ValueError("BBmerge was not found. check that the BBmerge reformat.sh package is executible")
+
+        # Deduplicate
+        sobj._deduplicate(threads=threads)
+        try:
+            # HMMSearch for ITS regions
+            hmmfile = os.path.join(ROOT_DIR, "ITSx_db", "HMMs", taxa_dict[taxa])
+            sobj._search(hmmfile=hmmfile, threads=threads)
+        except:
+            raise ValueError("hmmsearch was not found, make sure HMMER3 is installed and executible")
+
+        # Parse HMMseach output.
+        its_pos = itsx.ItsPosition(domtable=sobj.dom_file, region=region)
+        # Create deduplication object.
+        dedup_obj = itsx.Dedup(uc_file=sobj.uc_file, rep_file=sobj.rep_file, seq_file=sobj.seq_file)
+        results = SingleLanePerSampleSingleEndFastqDirFmt()
+        path = results.sequences.path_maker(sample_id="seq",
+                                            barcode_id=1,
+                                            lane_number=1,
+                                            read_number=1)
+
+        manifest_fn.write("seq,{},forward".format(path.name))
+        # Create trimmed sequences.
+        dedup_obj.create_trimmed_seqs(str(path), gzipped=True, itspos=its_pos)
+        # Deleting the temp files.
+        itsx.shutil.rmtree(sobj.tempdir)
     # Writing out the results.
+    manifest_fn.close()
     _write_metadata(results)
     results.manifest.write_data(manifest, FastqManifestFormat)
-    # Deleting the temp files.
-    itsx.shutil.rmtree(sobj.tempdir)
     return results
 # Separating the functions from the commands
 
