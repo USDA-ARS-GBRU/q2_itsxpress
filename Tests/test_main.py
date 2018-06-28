22141c05e909702d895c7bd8a7db0f589ae449e3
kweber1 <kweber1@ufl.edu>
2018-06-28 10:21:52 -0400

    Changed asserts

--- Tests/test_main.py
+++ Tests/test_main.py
@@ -14,7 +14,8 @@ def test_view_artifcat_type():
     testData = SingleLanePerSamplePairedEndFastqDirFmt(testFile,"r")
     os.chdir(str(testData))
     exp1 = itsxq._view_artifact_type()
-    assert "SampleData[PairedEndSequencesWithQuality]" in exp1
+    if not ("SampleData[PairedEndSequencesWithQuality]" in exp1):
+        raise AssertionError()
     testFile2 = os.path.join(TEST_DIR, "test_data","pairedbroken","50d5f31a-a761-4c04-990c-e7668fe6bf00","data")
     testData2 = SingleLanePerSamplePairedEndFastqDirFmt(testFile2,"r")
     os.chdir(str(testData2))
@@ -30,12 +31,15 @@ def test_fastq_id_maker():
     for sequence in exp1Set:
         expList.append(sequence[0])
         expList.append(sequence[1])
-    assert expList == ['4774-1-MSITS3_0_L001_R1_001.fastq.gz', '4774-1-MSITS3_1_L001_R2_001.fastq.gz']
-    assert exp2 == False
+    if not expList == ['4774-1-MSITS3_0_L001_R1_001.fastq.gz', '4774-1-MSITS3_1_L001_R2_001.fastq.gz']:
+        raise AssertionError()
+    if exp2 != False:
+        raise AssertionError()
 
 def test_taxa_prefix_to_taxa():
     exp1 = itsxq._taxa_prefix_to_taxa("A")
-    assert exp1 == "Alveolata"
+    if not (exp1 == "Alveolata"):
+        raise AssertionError()
 
 
 
